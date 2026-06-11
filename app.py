0c41e48920fbc5a647fed4024889dcc77ea5e594
leonardordeus <leonardo.deus@ceub.edu.br>
2026-06-11 17:48:22 +0000

    mudança perigosa no codigo

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 @app.route('/status')
 def status():
     # Código correto: retorna 200. Na aula, vamos quebrar isso mudando para 500.
-    return jsonify(status="Ambiente Saudável"), 200
+    return jsonify(status="Ambiente Saudável"), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
